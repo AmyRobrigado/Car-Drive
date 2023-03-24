c8178964178b0bd181a59dc6f3c9b0d31238dbc4
Amy Robrigado <amyantanod@gmail.com>
2023-03-24 14:05:16 -0400

    Adding JavaScript functionality

---
+++ scripts/magic.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    
+    // variables
+        $surface = $('.surface');
+        $car = $('.car'); 
+
+    // keypress event
+        $(document).on('keypress',function(e){
+            console.log(e.which)
+        })
+        
+}); 
